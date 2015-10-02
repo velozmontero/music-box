5a023ae001557c53c1b4a1ccd20757c7f611aeb7
velozmontero <velozmontero@gmail.com>
2015-10-01 22:01:27 -0400

    coje tu piano

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.ready(function(){
+$(document).ready(function(){
+    
     var cnote= document.getElementById("c-note");
     var dnote= document.getElementById("d-note");
     var enote= document.getElementById("e-note");
@@ -7,8 +8,39 @@ document.ready(function(){
     var anote= document.getElementById("a-note");
     var bnote= document.getElementById("b-note");
     
-   $('#c').on('click', function(){
-     cnote.currentTime=0;
-       cnote.play();
-   }); 
+    $('#c').on('click', function(){
+        cnote.currentTime=0;
+        cnote.play();
+    }); 
+   
+    $('#d').on('click', function(){
+        dnote.currentTime=0;
+        dnote.play();
+    });  
+    
+    $('#e').on('click', function(){
+        enote.currentTime=0;
+        enote.play();
+    });
+    
+    $('#f').on('click', function(){
+        fnote.currentTime=0;
+        fnote.play();
+    }); 
+    
+    $('#g').on('click', function(){
+        gnote.currentTime=0;
+        gnote.play();
+    }); 
+    
+    $('#a').on('click', function(){
+        anote.currentTime=0;
+        anote.play();
+    });
+    
+    $('#b').on('click', function(){
+        bnote.currentTime=0;
+        bnote.play();
+    }); 
+       
 });
